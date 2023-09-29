8aae67335d5c29d126f779090bc11db4917c5b87
aniketchoudhary34 <aniketchoudhary3435@gmail.com>
2023-09-29 10:34:02 +0530

    square

---
+++ square.py
@@ -0,0 +1,18 @@
+#n=4
+#i=1
+#square=0
+#while i<=n:
+    #square=i*2
+    #i=i+1
+#print(square)
+
+#n=5
+#for i in range(0,n+1):
+# square=i*2
+#print(square)
+
+n=10
+i=0
+for i in range (i,n+1):
+    print(i)
+    i=i+1
