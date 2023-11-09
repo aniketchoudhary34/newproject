3cdd2072cea4fc3ea64f5f0209d409589e95ac73
aniketchoudhary34 <aniketchoudhary3435@gmail.com>
2023-11-09 11:46:34 +0530

    hello

---
+++ fib.py
@@ -0,0 +1,37 @@
+#n=8
+#i=0
+#a=0
+#b=1
+#for i in range (n):
+#    if i == 0:
+#        print(0)
+#    elif i == 1:
+#        print(1)
+#    else:
+#        
+#        ans=a+b
+#        print(ans)    
+#        a=b
+#        b=ans       
+#        
+
+
+n=8
+i=0
+a=0
+b=1
+while i<=n:
+    if i == 0:
+        print(0)
+    elif i == 1:
+        print(1)
+    else:
+        
+        ans=a+b
+        print(ans)    
+        a=b
+        b=ans       
+    
+    i+=1
+
+    
